54aabe34d47a31eb6659f29a927a531e98799255
Yuya Tabata <yuya1212h4.066@gmail.com>
2017-11-24 14:24:35 +0900

    fix-carrierwave

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -3,18 +3,16 @@ require 'carrierwave/storage/file'
 require 'carrierwave/storage/fog'
 
 CarrierWave.configure do |config|
-  config.fog_credentials = {
-    provider: 'AWS',
-    aws_access_key_id: ENV['ACCESS_KEY_ID'],
-    aws_secret_access_key: ENV['SECRET_ACCESS_KEY'],
-    region: 'ap-northeast-1'
-  }
+  # config.fog_credentials = {
+  #   provider: 'AWS',
+  #   aws_access_key_id: ENV['ACCESS_KEY_ID'],
+  #   aws_secret_access_key: ENV['SECRET_ACCESS_KEY'],
+  #   region: 'ap-northeast-1'
+  # }
 
     case Rails.env
     when 'development'
-        config.storage = :fog
-        config.fog_directory  = 'sample-test-yuya1212'
-        config.asset_host = 'https://s3-ap-northeast-1.amazonaws.com/sample-test-yuya1212'
+      config.storage :file
     when 'production'
         config.storage = :fog
         config.fog_directory  = 'sample-test-yuya1212'
