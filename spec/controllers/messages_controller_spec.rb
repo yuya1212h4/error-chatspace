6b7f47fb5f578dea42fa3b09109ce175ead43fb8
Yuya Tabata <yuya1212h4.066@gmail.com>
2017-11-25 15:11:03 +0900

    1-8_q

--- spec/controllers/messages_controller_spec.rb
+++ spec/controllers/messages_controller_spec.rb
@@ -12,12 +12,12 @@ describe MessagesController, type: :controller do
 
   describe 'GET #new' do
     it "renders the :new template" do
-      get :new, params: { group_id: group.id }
+      get :new
       expect(response).to render_template :new
     end
 
     it "assigns the requested contact to @group" do
-      get :new, params: { group_id: group.id }
+      get :new
       expect(assigns(:group)).to eq group
     end
   end
